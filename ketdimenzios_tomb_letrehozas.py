da38303f0f7f69cac5f7fe155d59a07eb4a2aae9
Dávid Mezei <mezei.david.sandor@diak.szbi-pg.hu>
2026-03-11 11:31:23 +0100

    Fill 2D array with numbers; add XO printer

--- ketdimenzios_tomb_letrehozas.py
+++ ketdimenzios_tomb_letrehozas.py
@@ -2,11 +2,14 @@ tomb = []
 
 sor = 4
 oszlop = 3
+szam = 1
 
 for i in range(sor):
     sor_tomb = []
     for j in range(oszlop):
-        sor_tomb.append("...")
+        sor_tomb.append(szam)
+        szam += 1
     tomb.append(sor_tomb)
 
 print(tomb)
+
